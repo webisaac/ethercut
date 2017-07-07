060389cfd75b99ab79ea9942377ab8f6ff983d99
Ivan Luengo <evilgroot@gmail.com>
2017-07-07 15:33:31 +0200

    Moved platform constants to const.py

---
+++ ethercut/const.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+# ETHERCUT SUITE
+# Author: Ivan 'evilgroot' Luengo
+# Email: evilgroot@gmail.com
+
+# This project is released under a GPLv3 license
+
+"""
+Program constants
+"""
+
+import sys
+
+
+LINUX  = sys.platform.startswith("linux")
+DARWIN = sys.platform.startswith("darwin")
--- ethercut/platform/__init__.py
+++ ethercut/platform/__init__.py
@@ -10,11 +10,7 @@
 Platform dependencies
 """
 
-import sys
-
-
-LINUX  = sys.platform.startswith("linux")
-DARWIN = sys.platform.startswith("darwin")
+from ethercut.const import LINUX, DARWIN
 
 
 if LINUX:
@@ -22,3 +18,8 @@ if LINUX:
     enable_ip_forward = linux_enable_forward
     disable_ip_forward = linux_disable_forward
     check_ip_forward = linux_check_forward
+elif DARWIN:
+    from ethercut.platform.darwin import darwin_enable_forward, darwin_disable_forward, darwin_check_forward
+    enable_ip_forward = darwin_enable_forward
+    disable_ip_forward = darwin_disable_forward
+    check_ip_forward = darwin_check_forward
