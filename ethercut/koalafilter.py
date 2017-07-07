be13ad9fba8096f82c72c6f39e866beccdf9f659
Ivan Luengo <evilgroot@gmail.com>
2017-07-07 15:35:02 +0200

    Clean-up the code

--- ethercut/koalafilter.py
+++ ethercut/koalafilter.py
@@ -10,7 +10,7 @@
 Koala filter
 """
 
-import Queue
+import Queue, socket
 import ethercut.platform as platform
 import ethercut.exceptions as exceptions
 import ethercut.types.basethread as basethread
@@ -22,81 +22,160 @@ import scapy.layers.inet
 
 
 class KoalaFilter(object):
-    """
-    This class is responsible of filtering the packets
-    """
+
+    __slots__ = [ "stats", "eval_thread", "forward_thread",
+                  "decode_thread", "to_forward", "to_decode",
+                  "sniffed_packets", "decoder_manager",
+                  "from_file", "enabled", "running" ]
 
     def __init__(self, decmanager):
-        # Filter statistics
         self.stats = FilterStats()
-        # Queues to pass packets to the corresponding threads
+        self.decoder_manager = decmanager
         self.to_forward = Queue.Queue()
         self.to_decode = Queue.Queue()
+        # Need to be configured when the context is updated
         self.from_file = None
         self.sniffed_packets = None
-        self.decoder_manager = decmanager
+
+        self.enabled = False
+        self.running = False
+
+        # Threads that make up the filter activity
 
         # Packet evaluation thread
         self.eval_thread = basethread.BaseThread("Packet eval")
         self.eval_thread.run = self.eval_packets
 
-        # Tread with the filter activity
-        self.filter_thread = basethread.BaseThread("Filter")
-        self.filter_thread.run = self.filter_activity
+        # Packet forwarding thread
+        self.forward_thread = basethread.BaseThread("Packet forward")
+        self.forward_thread.run = self.forward_packets
 
-        # Packet decoding
+        # Packet decode thread
         self.decode_thread = basethread.BaseThread("Packet decode")
         self.decode_thread.run = self.decode_packets
 
-        self.enabled = False
-        self.running = False
 
     def configure(self):
         """
-        Configures the filter engine
+        This function configures the filter, the filter must be configured at least once
+        before running it (as it is disabled by default).
         """
+        # The filter can't be configured while it is running
         if self.running:
-            ctx.ui.warning("Can't configure the filter engine while it's running")
+            ctx.ui.warning("The filter can't be configured while it is running")
             return
+
         if not ctx.opt.sniff.sniff:
-            # No sniffed packets to filter :P
+            # There are no packets to filter :P
             self.enabled = False
-            ctx.ui.msg("Filter engine disabled")
+            ctx.ui.msg("Koala filter disabled")
+
         else:
-            # Check if we are reading from a file or we are live
-            self.from_file = ctx.opt.sniff.read
-            self.sniffed_packets = ctx.sniffed_packets
             self.enabled = True
+            self.sniffed_packets = ctx.sniffed_packets
 
-            if self.from_file:  # No packets to drop nor forward while reading from a file
+            # Configure the filter for live or offline sniffing
+            self.from_file = ctx.opt.sniff.read
 
-                drop = CStr("OFF").red
-                forward = CStr("OFF").red
+            if self.from_file:
+                # No packets to drop nor forward while sniffing offline!
+                drop = CStr("off").red
+                forward = CStr("off").red
 
-            elif ctx.opt.attack.unoffensive: # Let the kernel do the forwarding, we are just going to decode
-
+            elif ctx.opt.attack.unoffensive:
+                # Use kernel forwarding, this configuration won't allow the koala filter
+                # to manipulate the packets before forwarding them
                 platform.enable_ip_forward()
-                drop = CStr("KERNEL").green
-                forward = CStr("KERNEL").green
+                drop = CStr("kernel").green
+                forward = drop = CStr("kernel").green
 
-            elif ctx.opt.attack.kill: # Drop every packet that matches the filter
-
+            elif ctx.opt.attack.kill:
+                # Kill every connection
                 platform.disable_ip_forward()
-                drop = CStr("ON").green
-                forward = CStr("OFF").green
+                drop = CStr("on").green
+                forward = CStr("off").red
 
-            else: # Ethercut is responsible of forwarding/dropping the packets
-                # XXX: This feature is not working jet!!
-                raise exceptions.EthercutException("This filter configuration is not available jet :(")
+            else:
+                # The koala filter will handle dropping and forwarding
                 platform.disable_ip_forward()
-                drop = CStr("ON").green
-                forward = CStr("ON").green
+                drop = CStr("on").green
+                forward = CStr("on").green
 
-            decode = CStr("ON").green
-
+            decode = CStr("on").green
             ctx.ui.msg("Koala filter enabled | Dropping: %s | Forwarding: %s | Decoding: %s" %(drop, forward, decode))
 
-    def filter_activity(self):
+
+    def eval_packets(self):
+        """
+        Function that contains the activity of the evaluation thread.
+        This thread evaluates the packets from the sniffed queue and determines whether
+        they should be forwarded or dropped and decoded or ignored.
+        """
+        while self.running:
+            try:
+                packet = self.sniffed_packets.get(block=False, timeout=1)
+            except Queue.Empty:
+                continue
+
+            if packet is None:
+                # There are no more packets to filter
+                self.to_forward.put(None)
+                self.to_decode.put(None)
+                break
+            else:
+                # Add packet to statistics
+                self.stats.total += 1
+
+            drop = True
+            ignore = True
+
+            try:
+                # Don't drop packets that come from a file or that have our MAC address as
+                # the ethernet destination field and different IP address on the IP destination
+                # field (those packets must be forwarded)
+                if self.from_file or (ctx.iface.mac == packet.dst and ctx.iface.ip != packet.payload.dst):
+                    drop = False
+
+                    # Now check if we can decode the packet or it should be ignored. We have to
+                    # check if it complies with the TARGET specifications
+
+                    # Check from TARGET1 to TARGET2
+                    if (ctx.target1.check((packet.payload.src, packet.src, packet.sport)) and
+                    ctx.target2.check((packet.payload.dst, packet.dst, packet.dport))):
+                        ignore = False
+
+                    # Check from TARGET2 to TARGET1
+                    elif (ctx.target2.check((packet.payload.src, packet.src, packet.sport)) and
+                    ctx.target1.check((packet.payload.dst, packet.dst, packet.dport))):
+                        ignore = False
+
+            except AttributeError:
+                # The packet doesn't have an IP datagram or a TCP/UDP segment ecapsulated
+                pass
+
+            if not self.from_file:
+                if drop:
+                    self.stats.dropped += 1
+                else:
+                    self.to_forward.put(packet)
+
+            if not ignore:
+                self.to_decode.put(packet)
+            else:
+                self.stats.ignored += 1
+
+
+    def forward_packets(self):
+        """
+        This function represents the activity of packet forwarding.
+        Takes the packets from to_forward queue and sends them back to the wire at layer 3 to
+        their real destination.
+        """
+
+        # Use dnet to send the packets at layer 3
+        snd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        snd.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+
         while self.running:
             try:
                 packet = self.to_forward.get(block=False, timeout=1)
@@ -104,72 +183,19 @@ class KoalaFilter(object):
                 continue
 
             if packet is None:
-                self.to_decode.put(None)
+                # No more packets to forward
                 break
 
-            if not packet.eth_ignore:
-                # Pass it to the decoder chain
-                self.to_decode.put(packet)
-
-            else:
-                self.stats.ignored += 1
-
-            if not self.from_file:
-                self.stats.forwarded += 1
-
-
-    def eval_packets(self):
-        while self.running:
-            try:
-                packet = self.sniffed_packets.get(block=False, timeout=1)
-            except Queue.Empty:
-                continue
-
-            if packet == None:  # No more packets to filter
-                self.to_forward.put(None)
-                break
-            else:
-                self.stats.total += 1
-
-            drop = True
-            ignore = True
-
-            try:
-
-                # Don't drop the packet if we are reading from a file or the packet MAC address
-                # is ours but the IP address is different
-                if self.from_file or (packet.dst == ctx.iface.mac and packet.payload.src != ctx.iface.ip):
-                    drop = False
-
-                    # Now we have to check if the packet should be treated by ethercut or should
-                    # be ignored. We have to check if the packet complies with the target specifications
-
-                    # Check TARGET1 ---> TARGET2
-                    if (ctx.target1.check((packet.payload.src, packet.src, packet.sport))) and \
-                    ctx.target2.check((packet.payload.dst, packet.dst, packet.dport)):
-                        ignore = False
-
-                    # Check TARGET1 <--- TARGET2
-                    elif (ctx.target2.check((packet.payload.src, packet.src, packet.sport))) and \
-                    ctx.target1.check((packet.payload.dst, packet.dst, packet.dport)):
-                        ignore = False
-
-                # Update the stats
-                if drop:
-                    self.stats.dropped += 1
-
-                else:
-                    # Don't update the forwarded stats right now as the packet may be dropped by
-                    # the filters
-                    packet.eth_ignore = ignore
-                    self.to_forward.put(packet)
-
-            except AttributeError: # Packet has no IP, IPv6, UDP or TCP layer therefore we can't forward it
-                if not self.from_file:
-                    self.stats.dropped += 1
-
+            # Send the packet at layer 3 and let the kernel do the forwarding
+            snd.sendto(str(packet.payload), (packet.payload.dst, 0))
+            self.stats.forwarded += 1
 
     def decode_packets(self):
+        """
+        This function represents the activity of packet decoding.
+        Takes the packets from to_decode queue and pass them to the decoder manager that
+        will handle the parsing.
+        """
         while self.running:
             try:
                 packet = self.to_decode.get(block=False, timeout=1)
@@ -177,32 +203,33 @@ class KoalaFilter(object):
                 continue
 
             if packet is None:
+                # No more packets to decode
                 break
 
+            # Pass the packet to the decoder manager
             self.decoder_manager.decode(packet)
             self.stats.decoded += 1
 
-
     def start(self):
         """
-        Start the filter engine
+        Starts the koala filter activity
         """
         if not self.enabled or self.running:
             return
         self.running = True
         self.eval_thread.start()
-        self.filter_thread.start()
+        self.forward_thread.start()
         self.decode_thread.start()
 
     def stop(self):
         """
-        Stop the filter engine
+        Stops the koala filter activity
         """
-        if not self.running or not self.enabled:
+        if not self.enabled or not self.running:
             return
         self.running = False
         self.eval_thread.end()
-        self.filter_thread.end()
+        self.forward_thread.end()
         self.decode_thread.end()
 
 
